f3fefd169ff8adbf270569eaf6ee3520aac0d478
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-23 19:11:18 -0400

    Sale ViewModel

---
+++ java.lang.SqlException/Models/SaleViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Models
+{
+    public class SaleViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Account to Use")]
+        public int AccountId { get; set; }
+        [Required]
+        [Display(Name = "Advertisement")]
+        public int AdId { get; set; }
+        [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage="You must buy at least 1")]
+        [Display(Name = "Num to Buy")]
+        public int Num { get; set; }
+
+        public AdViewModel Ad { get; set; }
+        public List<SelectListItem> Accounts { get; set; }
+    }
+}
