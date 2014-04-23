d070901777d241c7f0d16e697addcab09e8a94f0
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-23 19:12:44 -0400

    Revenue Summary Listing ViewModel

---
+++ java.lang.SqlException/Models/RevenueSummaryListingViewmodel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Models
+{
+    public class RevenueSummaryListingViewmodel
+    {
+        public decimal TotalAmount { get; set; }
+        public string ItemName { get; set; }
+        public string ItemType { get; set; }
+        public string CustomerName { get; set; }
+        public string CompanyName { get; set; }
+
+    }
+}
