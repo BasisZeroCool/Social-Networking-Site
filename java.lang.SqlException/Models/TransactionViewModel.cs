c784b0fd3d78a71e4143939c7f7fbe05b279a9a2
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-23 07:22:06 -0400

    Transaction ViewModel

---
+++ java.lang.SqlException/Models/TransactionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Models
+{
+    public class TransactionViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Date of Sale")]
+        public DateTime DateOfSale { get; set; }
+        [Display(Name = "Number Bought")]
+        public int NumberOfUnits { get; set; }
+        [Display(Name = "Account")]
+        public int AccountNumber { get; set; }
+        [Display(Name = "Advertisement")]
+        public int AdId { get; set; }
+        public string Username { get; set; }
+
+        public bool SearchByUserName { get; set; }
+        public bool SearchByItemName { get; set; }
+        public string ItemName { get; set; }
+
+    }
+}
