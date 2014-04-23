beb42c71bfb973bb0a2372e4bfa37156819f9bfd
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-22 22:05:37 -0400

    Account History ViewModel

---
+++ java.lang.SqlException/Models/AccountHistoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace java.lang.SqlException.Models
+{
+    public class AccountHistoryViewModel
+    {
+        [Display(Name ="Date of Sale")]
+        public DateTime DateOfSale {get; set;}
+        [Display(Name = "Item Name")]
+        public String ItemName { get; set; }
+        public String Company { get; set; }
+        [Display(Name = "Number Bought")]
+        public int NumBought { get; set; }
+        [Display(Name = "Unit Price")]
+        public decimal UnitPrice { get; set; }
+    }
+}
