58848eda0646bc9098270cadcd4c649b7694827b
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-24 21:31:20 -0400

    RecordSale ViewModel

---
+++ java.lang.SqlException/Models/RecordSaleViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Models
+{
+    public class RecordSaleViewModel
+    {
+        [Display(Name = "Advertisement")]
+        public int AdId { get; set; }
+        [Display(Name = "Account")]
+        public int AccountId { get; set; }
+        [Display(Name = "Num Bought")]
+        public int NumUnits { get; set; }
+
+        public List<SelectListItem> Ads { get; set; }
+        public List<SelectListItem> Accounts { get; set; }
+    }
+}
