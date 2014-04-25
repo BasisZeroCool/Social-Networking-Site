31017815a21494dbc7fe33cb07e01bb683ede008
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-24 21:37:32 -0400

    List Of Sales Report ViewModel

---
+++ java.lang.SqlException/Models/ListOfSaleReportViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Models
+{
+    public class ListOfSaleReportViewModels 
+    {
+       public List<SalesReportViewModel> ListOfSalesReportViewModels = new List<SalesReportViewModel>();  
+
+    }
+}
