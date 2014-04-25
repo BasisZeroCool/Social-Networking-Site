7f4a29eeebf2720e447e9232f2f742490b3ee3f4
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-24 21:34:02 -0400

    List Of Customer Rep ViewModel

---
+++ java.lang.SqlException/Models/ListOfCustRepSumViewModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Models
+{
+    public class ListOfCustRepSumViewModels 
+    {
+        public List<CustRepRevenueViewModel> ListOfCustomerRepViewModels { get; set; } 
+    }
+}
