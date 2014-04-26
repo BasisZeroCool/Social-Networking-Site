44a6d32d90c0412f6bb7ef04b5fd5abcf491bf12
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-26 09:37:47 -0400

    List Of Employees ViewModel

---
+++ java.lang.SqlException/Models/ListOfEmployeesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Models
+{
+    public class ListOfEmployeesViewModel 
+    {
+        public List<EmployeeDashViewModel> ListOfEmps { get; set; }
+
+    }
+}
