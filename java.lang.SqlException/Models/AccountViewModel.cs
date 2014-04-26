fe25a9ee2b627fab5001f0c60102da36d7c2deee
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-26 09:45:30 -0400

    Account ViewModel

---
+++ java.lang.SqlException/Models/AccountViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Models
+{
+    public class AccountViewModel {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        
+      
+    }
+}
