35a080795c9d2f0d7598c666f74fa76d5695c1d8
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-26 09:36:33 -0400

    List Of Revenue ViewModel

---
+++ java.lang.SqlException/Models/ListOfRevenueViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Models
+{
+    public class ListOfRevenueViewModel
+    {
+        public List<RevenueSummaryListingViewmodel> ListOfRevenueSummaryListing { get; set; }
+
+    }
+}
