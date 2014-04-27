2610111121cc28027b8825c885f45b2dcaf32cd5
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-27 11:18:20 -0400

    Customer Representative Revenue ViewModel

---
+++ java.lang.SqlException/Models/CustRepRevenueViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Models
+{
+    public class CustRepRevenueViewModel
+    {
+        public int Id { get; set; }
+        public string CustRepName { get; set; }
+        public decimal GrandTotal { get; set; }
+
+    }
+}
