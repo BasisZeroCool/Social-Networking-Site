c24a3bb6adeef568fc1bb73a0d57f4bc07cc09ae
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-28 10:43:29 -0400

    Customer ViewModel

---
+++ java.lang.SqlException/Models/CustomerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace java.lang.SqlException.Models
+{
+    public class CustomerViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("First Name")]
+        public String FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public String LastName { get; set; }
+        [DisplayName("Uername")]
+        public String Username { get; set; }
+        public int? Rating { get; set; }
+    }
+}
