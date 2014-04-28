559a0c5a22928fce17b11c50ecf282a9941ffe1d
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-28 16:50:05 -0400

    Purchase Controller

---
+++ java.lang.SqlException/Controllers/PurchaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Controllers
+{
+    public class PurchaseController : BaseController
+    {
+        //
+        // GET: /Purchase/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
