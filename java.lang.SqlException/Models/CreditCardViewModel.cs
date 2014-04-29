1279e636ed5c4529cf77369e61a9bf3859e66e44
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-29 18:49:54 -0400

    CreditCard ViewModel

---
+++ java.lang.SqlException/Models/CreditCardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace java.lang.SqlException.Models
+{
+    public class CreditCardViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Card Number")]
+        public string CardNumber { get; set; }
+        [DisplayName("Date Added")]
+        public DateTime DateAdded { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
