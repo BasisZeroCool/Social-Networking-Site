c33efcab25d824045ab824e4b8e934999d1f5e78
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-29 18:53:53 -0400

    Message ViewModel

---
+++ java.lang.SqlException/Models/MessageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Models
+{
+    public class MessageViewModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Subject")]
+        [Required]
+        public string Subject { get; set; }
+
+        [DisplayName("Message")]
+        [Required]
+        public string Content { get; set; }
+
+        [DisplayName("Date")]
+        public DateTime Date { get; set; }
+
+        [DisplayName("From")]
+        public string Sender { get; set; }
+
+        [DisplayName("To")]
+        public string Receiver { get; set; }
+
+        [DisplayName("To")]
+        [Required]
+        public int ReceiverId { get; set; }
+
+
+        public List<SelectListItem> MessageRecipients { get; set; }
+
+    }
+}
