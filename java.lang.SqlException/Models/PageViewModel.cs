874830e6f8b76deed96cc5c2052aff5a6cb4647e
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-29 18:55:12 -0400

    Page ViewModel

---
+++ java.lang.SqlException/Models/PageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace java.lang.SqlException.Models
+{
+    public class PageViewModel
+    {
+        public int Id { get; set; }
+
+        public int PostCount { get; set; }
+
+        public int CircleId { get; set; }
+
+        public String CircleName { get; set; }
+
+        public String CircleType { get; set; }
+
+        public List<PostViewModel> Posts { get; set; }
+    }
+}
