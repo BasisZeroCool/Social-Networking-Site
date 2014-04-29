d3c2ccb87b119ae4b32599f676cd0ce3b2691a9c
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-29 18:50:54 -0400

    Circle ViewModel

---
+++ java.lang.SqlException/Models/CircleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace java.lang.SqlException.Models
+{
+    public class CircleViewModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Name")]
+        [Required(ErrorMessage = "A Circle Name is required.")]
+        public string CircleName { get; set; }
+
+        [DisplayName("Type")]
+        public string CircleType { get; set; }
+
+        [DisplayName("Owner")]
+        public string OwnerName { get; set; }
+
+    }
+}
