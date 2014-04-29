e0dacd33e46d8059c8ce2dd453bc2dc2cb4a9fae
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-29 18:54:57 -0400

    Comment ViewModel

---
+++ java.lang.SqlException/Models/CommentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace java.lang.SqlException.Models
+{
+    public class CommentViewModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Date Added")]
+        public DateTime DateAdded { get; set; }
+
+        [DisplayName("Content")]
+        public string Content { get; set; }
+
+        public int PostId { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public String Author { get; set; }
+
+        public bool UserLikes { get; set; }
+
+        public bool UserCanEdit { get; set; }
+    }
+}
