390cd16c93bf028339119d9ebd4d0f2293e8bbb7
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-29 18:48:01 -0400

    Guide Controller

---
+++ java.lang.SqlException/Controllers/GuideController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Controllers
+{
+    public class GuideController : Controller
+    {
+        //
+        // GET: /Guide/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
