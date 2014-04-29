3f19cd789708b80ce50057e82a001c2ae36587f1
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-29 18:48:31 -0400

    Help Controller

---
+++ java.lang.SqlException/Controllers/HelpController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace java.lang.SqlException.Controllers
+{
+    public class HelpController : Controller
+    {
+        public ActionResult CreateCircleHelp()
+        {
+            return View();
+        }
+
+        public ActionResult BuySomething()
+        {
+            return View();
+        }
+
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+
+        public ActionResult AddUserToCircle()
+        {
+            return View();
+        }
+
+        public ActionResult ManagerHelp()
+        {
+            return View();
+        }
+
+        public ActionResult CustomerHelp()
+        {
+            return View();
+        }
+
+        public ActionResult CustomerRepHelp()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
